b9325984b4df65a941e84041e02df2a4f11922e1
taeho_kim <rlaxogh4896@gmail.com>
2025-04-03 15:22:04 +0900

    tokenizer base

---
+++ tokenizer.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+
+import torch
+
+import tiktoken
+from tiktoken.load import load_tiktoken_bpe
+from types import SimpleNamespace
+
+class Tokenizer:
+    def __init__(self, model_path):
+        
+    def encode():
+
+
+    def decode():
+        
