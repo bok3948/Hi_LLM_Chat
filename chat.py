41684c917805614305a4eb43f96c749af8b77eea
taeho_kim <rlaxogh4896@gmail.com>
2025-04-04 10:11:59 +0900

    chat init

--- chat.py
+++ chat.py
@@ -1,6 +1,9 @@
 
 class chatbot:
-    def __init__(self, model,  max_seq_len, tokenizer,  device):
+    def __init__(self, model, tokenizer, device):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.device = device
     def generate():
     def decode_n_tokens():
     def prefill():
