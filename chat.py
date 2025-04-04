02569c226daf48c9ffd3a781e2559b9709f5a3c7
taeho_kim <rlaxogh4896@gmail.com>
2025-04-04 10:13:18 +0900

    chat prefill

--- chat.py
+++ chat.py
@@ -1,4 +1,7 @@
 
+import torch
+import logging
+
 class chatbot:
     def __init__(self, model, tokenizer, device):
         self.model = model
@@ -6,6 +9,16 @@ class chatbot:
         self.device = device
     def generate():
     def decode_n_tokens():
-    def prefill():
+    def prefill(
+        self,
+        model,
+        x: torch.Tensor,
+        input_pos: torch.Tensor,
+    ) -> torch.Tensor:
+        logger.debug("x: %s, input_pos: %s", x, input_pos)
+        logits = model(x, input_pos)
+        if self.generate_full_logit:
+            logits = logits[:, -1, :]
+        return self.sample(logits)[0]
     def chat():
         
